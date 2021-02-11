43f7b7617a24f6050934568b6af90d7adcdecad8
LecomteAdrien <lecomteadrien@hotmail.fr>
2021-02-11 16:42:27 +0100

    Clear unused entity

--- src/main/java/com/veterinary/veterinary/dosage/DosageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.veterinary.veterinary.dosage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "Dosages")
-public interface DosageRepository extends JpaRepository<Dosage, Integer> {
-}
